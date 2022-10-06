c30472e2aa6edb92f99a33e376ee5adda825e8c0
Merve <152120171025@ogrenci.ogu.edu.tr>
2022-10-06 17:51:47 +0300

    Data types example

---
+++ Homeworks1/dataTypes/src/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+	public static void main(String[] args) {
+		int number_a=12; //
+		byte number_b=127;
+		short number_c=10000;
+		long number_d=100;
+		float number_e=3.14f;
+		double number_f=12.5;
+	    char character='M';
+	    boolean real=true;
+	}
+}

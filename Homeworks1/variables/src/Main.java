2addbf68a0d5cd2617bbef58de45de49731c5645
Merve <152120171025@ogrenci.ogu.edu.tr>
2022-10-06 15:48:20 +0300

    integer and string variables

---
+++ Homeworks1/variables/src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+	public static void main(String[] args) {
+		int studentNumber=12;
+		String mesaj="Student number: ";
+		System.out.println(mesaj + studentNumber);
+		System.out.println("Student number: " + studentNumber);
+	}
+}

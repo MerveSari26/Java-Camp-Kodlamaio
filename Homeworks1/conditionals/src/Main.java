31edb1d12fdd9ed146bd8519c2db0660839715f7
Merve <152120171025@ogrenci.ogu.edu.tr>
2022-10-06 19:03:08 +0300

    if conditionals example

---
+++ Homeworks1/conditionals/src/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+	public static void main(String[] args) { //The program  to compare  20 and the number 
+		int number=19; 
+		if(number<20) {
+			System.out.println("Number is less than 20");
+		}
+		else if(number==20){
+			System.out.println("Number is equal to 20");
+		}
+		else {
+			System.out.println("Number is greater than 20");
+		}
+	}
+}
